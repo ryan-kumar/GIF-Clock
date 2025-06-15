20014cdb5c579b247fb422d32854eb969aad734f
Ryan Kumar <146591119+ryan-kumar@users.noreply.github.com>
2025-06-15 00:09:25 -0400

    Update display.cpp

--- display.cpp
+++ display.cpp
@@ -31,12 +31,27 @@ void displayTime() {
 }
 
 void typeMessage(String message) {
+  tft.fillScreen(ILI9341_BLACK);
   tft.setTextSize(2);
-  tft.setCursor(10,10);
+  tft.setCursor(10,100);
   for (int i = 0; i < message.length(); ++i) {
     tft.print(message[i]);
     delay(75);
   }
   delay(1000);
-  tft.fillScreen(ILI9341_BLACK);
+}
+
+void bootMessage() {
+  typeMessage("Yaaaaaawwwwwnnn......");
+  typeMessage("I was asleep for quite some time");
+  typeMessage("Can you tell me what time it is now?");
+  String time = getTime();
+  tft.setTextSize(4);
+  tft.setCursor(30, 100);
+  tft.println("+  +  +  +");
+  tft.println(time);
+  tft.println("-  -  -  -");
+  tft.drawRect(60, 170, 30, 10, ILI9341_PINK);
+  bool submitted = false;
+  while (!submitted) {}
 }
